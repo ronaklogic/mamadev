ad7d44b79186703bd64a5a7e4b19f9fe8be296b3
ronaklogic <ronak.logicalstreet@gmail.com>
2022-01-28 14:55:15 +0530

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 require "active_support/core_ext/integer/time"
-validates :email, presence: true
-
+  
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
